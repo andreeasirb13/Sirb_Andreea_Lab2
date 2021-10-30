e055d99e134137a0374fac346a64f6f28e3e9c0c
Andreea Sîrb <andreea.sirb13@yahoo.com>
2021-10-30 15:38:05 +0300

    lab4

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Sirb_Andreea_Lab2
 {
     /// <summary>
@@ -23,7 +24,31 @@ namespace Sirb_Andreea_Lab2
         public MainWindow()
         {
             InitializeComponent();
+
+            //creare obiect binding pentru comanda
+            CommandBinding cmd1 = new CommandBinding();
+            //asociere comanda
+            cmd1.Command = ApplicationCommands.Print;
+            //input gesture: I + Alt
+            ApplicationCommands.Print.InputGestures.Add(new KeyGesture(Key.I, ModifierKeys.Alt));
+            //asociem un handler
+            cmd1.Executed += new ExecutedRoutedEventHandler(CtrlP_CommandHandler);
+            //adaugam la colectia CommandBindings
+            this.CommandBindings.Add(cmd1);
+
+
+
+            //Doughnuts>Stop
+            //comanda custom
+            CommandBinding cmd2 = new CommandBinding();
+            cmd2.Command = CustomCommands.StopCommand.Launch;
+            cmd2.Executed += new ExecutedRoutedEventHandler(CtrlS_CommandHandler);//asociem handler
+            this.CommandBindings.Add(cmd2);
         }
+
+       
+
+
         private DoughnutMachine myDoughnutMachine;
 
 
@@ -31,6 +56,11 @@ namespace Sirb_Andreea_Lab2
         {
             myDoughnutMachine = new DoughnutMachine();
             myDoughnutMachine.DoughnutComplete += new DoughnutMachine.DoughnutCompleteDelegate(DoughnutCompleteHandler);
+
+            //binding la ob de tip Combobox(cmbType) cu colectia PriceList, pt a vizualiza tipurile de gogosi in ob CmbType
+            cmbType.ItemsSource = PriceList;
+            cmbType.DisplayMemberPath = "Key";
+            cmbType.SelectedValuePath = "Value";
         }
 
         private int mRaisedGlazed;
@@ -68,6 +98,8 @@ namespace Sirb_Andreea_Lab2
                     txtVanillaFilled.Text = mFilledVanilla.ToString();
                     break;
 
+    
+
             }
         }
 
@@ -81,13 +113,15 @@ namespace Sirb_Andreea_Lab2
         private void sugarToolStripMenuItem_Click(object sender, RoutedEventArgs e)
         {
             sugarToolStripMenuItem.IsChecked = true;
-            sugarToolStripMenuItem.IsChecked = false;
+            glazedToolStripMenuItem.IsChecked = false;
             myDoughnutMachine.MakeDoughnuts(DoughnutType.Sugar);
         }
 
         private void stopToolStripMenuItem_Click(object sender, RoutedEventArgs e)
         {
             myDoughnutMachine.Enabled = false;
+            this.Title = " Virtual Doughtnuts Factory ";
+            e.Handled = true;
         }
 
         private void exitToolStripMenuItem_Click(object sender, RoutedEventArgs e)
@@ -104,6 +138,167 @@ namespace Sirb_Andreea_Lab2
             }
 
         }
+
+        
+
+        // Handler pt eveniment rutat Click asociat controlului de tip MenuItem cu Header-Filled
+        private void FilledItems_Click(object sender, RoutedEventArgs e)
+        {
+            string DoughnutFlavour;
+
+            MenuItem SelectedItem = (MenuItem)e.OriginalSource;
+            DoughnutFlavour = SelectedItem.Header.ToString();
+
+            Enum.TryParse(DoughnutFlavour, out DoughnutType myFlavour);
+            myDoughnutMachine.MakeDoughnuts(myFlavour);
+        }
+
+
+
+        // handler la nivelul Grid-ului - titlul formul. se modif in f de tipul de gogoasa care se coace
+        private void FilledItemsShow_Click(object sender, RoutedEventArgs e)
+        {
+            string mesaj;
+
+            MenuItem SelectedItem = (MenuItem)e.OriginalSource;
+            mesaj = SelectedItem.Header.ToString() + " doughnuts are being cooked!";
+            this.Title = mesaj;
+        }
+
+
+        // o colectie de tipul key/value - pt a asocia fiecarui tip de gogoasa un pret
+        KeyValuePair<DoughnutType, double>[] PriceList = {
+             new KeyValuePair<DoughnutType, double>(DoughnutType.Sugar, 2.5),
+             new KeyValuePair<DoughnutType, double>(DoughnutType.Glazed,3),
+             new KeyValuePair<DoughnutType, double>(DoughnutType.Chocolate,4.5),
+             new KeyValuePair<DoughnutType, double>(DoughnutType.Vanilla,4),
+             new KeyValuePair<DoughnutType, double>(DoughnutType.Lemon,3.5)
+             };
+
+
+        // definim variabila de tip DoughnutType pt tipul de gogoasa curent selectat
+        DoughnutType selectedDoughnut;
+
+
+        // handler pt evenimentul SelectionChanged - ComboBox - sa se afiseze automat pretul gogosii 
+        private void cmbType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txtPrice.Text = cmbType.SelectedValue.ToString();
+            KeyValuePair<DoughnutType, double> selectedEntry = (KeyValuePair<DoughnutType, double>)cmbType.SelectedItem;
+            selectedDoughnut = selectedEntry.Key;
+        }
+
+        
+        // metoda pentru a valida cantitatea cumparata sa fie in stoc
+        private int ValidateQuantity(DoughnutType selectedDoughnut)
+        {
+            int q = int.Parse(txtQuantity.Text);
+            int r = 1;
+
+            switch (selectedDoughnut)
+            {
+                case DoughnutType.Glazed:
+                    if (q > mRaisedGlazed)
+                        r = 0;
+                    break;
+                case DoughnutType.Sugar:
+                    if (q > mRaisedSugar)
+                        r = 0;
+                    break;
+                case DoughnutType.Chocolate:
+                    if (q > mFilledChocolate)
+                        r = 0;
+                    break;
+                case DoughnutType.Lemon:
+                    if (q > mFilledLemon)
+                        r = 0;
+                    break;
+                case DoughnutType.Vanilla:
+                    if (q > mFilledVanilla)
+                        r = 0;
+                    break;
+            }
+            return r;
+        }
+
+
+        //handler pt evenim btnAdd_Click
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateQuantity(selectedDoughnut) > 0)
+            {
+                lstSale.Items.Add(txtQuantity.Text + " " + selectedDoughnut.ToString() +
+               ":" + txtPrice.Text + " " + double.Parse(txtQuantity.Text) *
+               double.Parse(txtPrice.Text));
+            }
+            else
+            {
+                MessageBox.Show("Cantitatea introdusa nu este disponibila in stoc!");
+            }
+        }
+
+        //handler pt evenim btnRemoveItem_Click - stergere din lstSale inregistrarea curenta la apasarea butonului RemoveItem
+        private void btnRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            lstSale.Items.Remove(lstSale.SelectedItem);
+        }
+
+
+        //handler de evenim btnCheckOut_Click - costul total in txtTotal si scadere cos
+        private void btnCheckOut_Click(object sender, RoutedEventArgs e)
+        {
+            txtTotal.Text = (double.Parse(txtTotal.Text) + double.Parse(txtQuantity.Text)
+            * double.Parse(txtPrice.Text)).ToString();
+
+            foreach (string s in lstSale.Items)
+            {
+                switch (s.Substring(s.IndexOf(" ") + 1, s.IndexOf(":") - s.IndexOf(" ") -1))
+                {
+                    case "Glazed":
+                        mRaisedGlazed = mRaisedGlazed - Int32.Parse(s.Substring(0, s.IndexOf(" ")));
+                        txtGlazedRaised.Text = mRaisedGlazed.ToString();
+                        break;
+
+                    case "Sugar":
+                        mRaisedSugar = mRaisedSugar - Int32.Parse(s.Substring(0, s.IndexOf(" ")));
+                        txtSugarRaised.Text = mRaisedSugar.ToString();
+                        break;
+
+                    case "Chocolate":
+                        mFilledChocolate = mFilledChocolate - Int32.Parse(s.Substring(0, s.IndexOf(" ")));
+                        txtChocolateFilled.Text = mFilledChocolate.ToString();
+                        break;
+
+                    case "Lemon":
+                        mFilledLemon = mFilledLemon - Int32.Parse(s.Substring(0, s.IndexOf(" ")));
+                        txtLemonFilled.Text = mFilledLemon.ToString();
+                        break;
+
+                    case "Vanilla":
+                        mFilledVanilla = mFilledVanilla - Int32.Parse(s.Substring(0, s.IndexOf(" ")));
+                        txtVanillaFilled.Text = mFilledVanilla.ToString();
+                        break;
+                }
+            }
+        }
+
+
+        //Comenzi predefinite - handler, se va executa la invocarea comenzii ApplicationCommands.Print in aplicatie
+        private void CtrlP_CommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("You have in stock:" + mRaisedGlazed + " Glazed," + mRaisedSugar +
+           "Sugar, " + mFilledLemon + " Lemon, " + mFilledChocolate + " Chocolate, " + mFilledVanilla + " Vanilla");
+        }
+
+        // Comenzi custom
+        private void CtrlS_CommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            //handler pentru comanda Ctrl+S -> se va executa stopToolStripMenuItem_Click
+            MessageBox.Show("Ctrl+S was pressed! The doughnut machine will stop!");
+            this.stopToolStripMenuItem_Click(sender, e);
+        }
+
+
     }
 
 
